8ac48c09da9af28864a3bbba229a8e73ecdc26c7
Arief Setyo <senuarioo@gmail.com>
2024-12-05 22:32:33 +0700

    last update

--- js/script.js
+++ js/script.js
@@ -1,18 +1,23 @@
 
-console.log('hola');
+function validateForm() {
+    let inputBeratBadan = document.getElementById('berat-badan-input').value;
+    let inputUsia = document.getElementById('usia-input').value;
+    let inputTinggiBadan = document.getElementById('tinggi-badan-input').value;
 
-function getBeratBadan() {
-    console.log(document.getElementById('berat-badan-input').value);
-}
+    console.log('BB:' + inputBeratBadan);
+    console.log('Usia:' + inputUsia);
+    console.log('Tinggi:' + inputTinggiBadan);
+    if (inputBeratBadan != '' && inputUsia != '' && inputTinggiBadan != '') {
+        let calculate = parseInt(inputBeratBadan) / parseInt (inputTinggiBadan) * (inputTinggiBadan);
+        updateResult (calculate);
+     } else {
+                alert ('Data tidak benar, Mohon cek kembali !!');
+            
+        }
+    }    
 
-
-
-function getUsia() {
-    console.log(document.getElementById('usia-input').value);
-}
-
-
-
-function getTinggiBadan() {
-    console.log(document.getElementById('tinggi-badan-input').value);
-}
+    function updateResult(result) {
+        document.getElementById('result-calculation').innerHTML = result;
+    }
+    
+    
