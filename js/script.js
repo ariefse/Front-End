acf73046ce8919ca088aadd617e1ec6f46219df2
Arief Setyo <senuarioo@gmail.com>
2024-12-05 16:38:49 +0700

    update mini project

---
+++ js/script.js
@@ -0,0 +1,18 @@
+
+console.log('hola');
+
+function getBeratBadan() {
+    console.log(document.getElementById('berat-badan-input').value);
+}
+
+
+
+function getUsia() {
+    console.log(document.getElementById('usia-input').value);
+}
+
+
+
+function getTinggiBadan() {
+    console.log(document.getElementById('tinggi-badan-input').value);
+}
